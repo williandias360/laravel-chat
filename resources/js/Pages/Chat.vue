183b385ee30e74a969f4ff8cb0af3b7c15b3eddb
Willian Dias Brito <willian.dias360@hotmail.com>
2020-12-24 11:04:58 -0300

    Commit inicial

---
+++ resources/js/Pages/Chat.vue
@@ -0,0 +1,162 @@
+<template>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Chat</h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div
+          class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex"
+          style="min-height: 400px; max-height: 400px"
+        >
+          <div
+            class="w-3/12 bg-gray-200 bg-opacity-25 border-r border-gray-200 overflow-y-scroll"
+          >
+            <!-- listuser -->
+            <ul>
+              <li
+                v-for="user in users"
+                :key="user.id"
+                :class="
+                  userActive && userActive.id == user.id
+                    ? 'bg-gray-200 bg-opacity-50'
+                    : ''
+                "
+                @click="loadMessages(user.id)"
+                class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer"
+              >
+                <p class="flex items-center">
+                  {{ user.name }}
+                  <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                </p>
+              </li>
+            </ul>
+          </div>
+
+          <!-- box message -->
+          <div class="w-9/12 flex flex-col justify-between">
+            <!-- mensagens-->
+            <div class="w-full p-6 flex-col overflow-y-scroll">
+              <div
+                v-for="message in messages"
+                :key="message.id"
+                :class="message.from == $page.auth.user.id ? 'text-right' : ''"
+                class="w-full mb-3 message"
+              >
+                <p
+                  :class="
+                    message.from == $page.auth.user.id
+                      ? 'messageFromMe'
+                      : 'messageToMe'
+                  "
+                  class="inline-block p-2 rounded-mb"
+                  style="max-width: 75%"
+                >
+                  {{ message.content }}
+                </p>
+                <span class="block mt-1 text-xs text-gray-500">{{
+                  message.created_at | formatDate
+                }}</span>
+              </div>
+            </div>
+
+            <!-- form -->
+            <div
+              v-if="userActive"
+              class="w-full bg-gray-200 bg-opacity-25 p-6 border-t border-gray-200"
+            >
+              <form v-on:submit.prevent="sendMessage">
+                <div
+                  class="flex rounded-md overflow-hidden border border-gray-300"
+                >
+                  <input
+                    v-model="message"
+                    type="text"
+                    class="flex-1 px-4 py-2 text-sm focus:outline-none"
+                  />
+                  <button
+                    type="submit"
+                    class="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2"
+                  >
+                    Enviar
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import axios from "axios";
+
+export default {
+  components: {
+    AppLayout,
+  },
+  data() {
+    return {
+      users: [],
+      messages: [],
+      userActive: null,
+      message: "",
+    };
+  },
+  mounted() {
+    axios.get("api/users").then((response) => {
+      this.users = response.data.users;
+    });
+  },
+  methods: {
+    scrollToBottom() {
+      if (this.messages.length) {
+        document.querySelectorAll(".message:last-child")[0].scrollIntoView();
+      }
+    },
+    async loadMessages(userId) {
+      axios.get(`api/users/${userId}`).then((response) => {
+        this.userActive = response.data.user;
+      });
+
+      await axios.get(`api/messages/${userId}`).then((response) => {
+        this.messages = response.data.messages;
+      });
+
+      this.scrollToBottom();
+    },
+    async sendMessage() {
+      await axios
+        .post("api/messages/store", {
+          content: this.message,
+          to: this.userActive.id,
+        })
+        .then((response) => {
+          this.messages.push({
+            from: "1",
+            to: this.userActive.id,
+            content: this.message,
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+          });
+          this.message = "";
+        });
+      this.scrollToBottom();
+    },
+  },
+};
+</script>
+
+<style>
+.messageFromMe {
+  @apply bg-indigo-300 bg-opacity-25;
+}
+
+.messageToMe {
+  @apply bg-gray-300 bg-opacity-25;
+}
+</style>
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Api\MessageController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::get("/users", [UserController::class, "index"])->name("users.index");
+    Route::get("/users/{user}", [UserController::class, "show"])->name("user.show");
+    Route::get('/messages/{user}', [MessageController::class, "listMessages"])->name("message.listMessages");
+    Route::post('/messages/store', [MessageController::class, "store"])->name("message.store");
+});
